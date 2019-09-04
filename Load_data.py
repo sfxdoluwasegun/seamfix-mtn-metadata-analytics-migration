689e7e550d5e39b71a589f0a23c612ffa8fa8fb7
jacintaE <jejiofor@seamfix.com>
2019-09-04 17:55:26 +0100

    #DSC-350 This file showcases the python code that is meant to read each JSON object from the provided file, flatten it and save to a list. This list is then split it into chunks,  loaded into a pandas dataframe and then eventually saved to the local machine for future reference.

--- Load_data.py
+++ Load_data.py
@@ -1,20 +1,24 @@
 # Necessary libraries to be installed are: dask, flatten_json, json(comes pre-installed)
 
-import json
-import dask.dataframe as dd
-import dask.delayed as delayed
-import flatten_json
-from flatten_json import flatten
-print('All libraries in')
-data = []
-with open('METRIC_DATA_REPORT_NEW_view.json') as f:
-    for line in f:
-        data.append(json.loads(line))
-print('... JSON object already read into the data object')
-dic_flattened = [flatten(d) for d in data]
-print('... data has been flattened successfully')
-z = delayed(dic_flattened)
-print('Flattened data has been moved to a delayed object')
-df = dd.from_delayed(z)
-df.to_csv('metadata_file-*.csv', index = False)
-
+if __name__ == '__main__':
+    import json
+    import pandas as pd
+    import flatten_json
+    from flatten_json import flatten
+    print('All libraries in')
+    data = []
+    with open('METRIC_DATA_REPORT_NEW_view.json') as f:
+        for line in f:
+            data.append(json.loads(line))
+    print('... JSON object already read into the data object')
+    flattened_list = [flatten(d) for d in data]
+    print('... Data has been flattened successfully')
+    first_set = flattened_list[0:20000]
+    df = pd.DataFrame(first_set)
+    df.to_csv('part1.csv', index=False)
+    print('... first batch of meta data records have been saved to the local drive')
+    second_set = flattened_list[20000:]
+    df = pd.DataFrame(second_set)
+    df.to_csv('part2.csv', index = False)
+    print('... second batch of meta data records have been saved to the local drive')
+    print('--------- End of code ---------')
