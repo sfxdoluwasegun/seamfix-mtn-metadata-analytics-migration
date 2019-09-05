a0b9913c9446b8f48dcdea586d4ddb4dab08902b
Fortune Okorji <fokorji@seamfix.com>
2019-09-05 18:01:17 +0100

    #DSC-389 Implemented a function to convert dataframe columns containing categorical data (string types) to non-categorical data (double types).

--- src/feature_scaling.py
+++ src/feature_scaling.py
@@ -1,14 +1,18 @@
 # Databricks notebook source
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.functions import *
-
+from pyspark.ml.feature import PCA, StringIndexer, VectorAssembler, OneHotEncoder, IndexToString
+from pyspark.ml.linalg import SparseVector, Vectors
+from pyspark.sql.types import *
+from pyspark.ml import Pipeline
 
 # COMMAND ----------
-# The ACCESS_KEY & SECRET_KEY could change due to monthly security modifications
+
+# The ACCESS_KEY & SECRET_KEY could change due to monthly security modifications. 
 BUCKET_NAME = "seamfix-machine-learning"
-ROOT_DIR = "s3://seamfix-machine-learning/mtn_xmlmetadata/METRIC_DATA_REPORT_NEW_view.json"
 ACCESS_KEY = "AKIAU7APHSU5V6VGK7XV"
 SECRET_KEY = "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"
+temp_path = "s3://seamfix-machine-learning/mtn_xmlmetadata"
 
 sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", ACCESS_KEY)
 sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", SECRET_KEY)
@@ -19,27 +23,15 @@ liveliness_df = spark.read.json("s3n://seamfix-machine-learning/mtn_xmlmetadata/
 portrait_df = spark.read.json("s3n://seamfix-machine-learning/mtn_xmlmetadata/portrait_refined/*.json")
 fingerprints_df = spark.read.json("s3n://seamfix-machine-learning/mtn_xmlmetadata/fingerprints_refined/*.json")
 usecase_df = spark.read.json("s3n://seamfix-machine-learning/mtn_xmlmetadata/useCase_refined/*.json")
-
-
-# COMMAND ----------
-
-root_df = spark.read.json("s3n://seamfix-machine-learning/mtn_xmlmetadata/root/*.json")
+# root_df = spark.read.json("s3n://seamfix-machine-learning/mtn_xmlmetadata/root/*.json")
 
 # COMMAND ----------
 
+# df = spark.read.json("dbfs:/mnt/S3/mtn_xmlmetadata/METRIC_DATA_REPORT_NEW_view.json")
+# df.createOrReplaceTempView("datasql")
 
-df = spark.read.json("dbfs:/mnt/S3/mtn_xmlmetadata/METRIC_DATA_REPORT_NEW_view.json")
-df.createOrReplaceTempView("datasql")
-
-captureDate = spark.sql("""select METRIC_DATA.captureDate.`$date` as captureDate from datasql""") 
-
-
-
-
-
-# COMMAND ----------
-
-get_unique_items(captureDate)
+# captureDate = spark.sql("""select METRIC_DATA.captureDate.`$date` as captureDate from datasql""") 
+# get_unique_items(captureDate)
 
 # COMMAND ----------
 
@@ -62,8 +54,125 @@ for item in df_list:
 
 # COMMAND ----------
 
-# liveliness_df.select('hashID').show()
+def one_hot_encoder(col_name: str, df: DataFrame) -> DataFrame:
+  stringIndexer = StringIndexer(inputCol=col_name, outputCol="{}_{}".format(col_name, "doub"))
+  decimalModel = stringIndexer.fit(df)
+  str_double = decimalModel.transform(df)
+  encoder = OneHotEncoder(inputCol="{}_{}".format(col_name, "doub"), outputCol="{}_{}".format(col_name, "ohe"))
+  ohe = encoder.transform(str_double)
+  
+  onehotEncoderPath = temp_path + "/onehot-encoder"
+  encoder.save(onehotEncoderPath)
+  
+  stringIndexerPath = temp_path + "/ohe-model"
+  stringIndexer.save(stringIndexerPath)
+  return str_double, ohe
+
+# COMMAND ----------
+
+df.select(df.columns[-2]).show()
+
+# COMMAND ----------
+
+# df1, df2 = one_hot_encoder(portrait_df.columns[5], portrait_df)
+# df.select("ptProperty_idx").show()
+stringIndexerPath = temp_path + "/ohe-model"
+stringIndexer = StringIndexer.load(stringIndexerPath)
+convertor = IndexToString(inputCol='df.columns[-2]', outputCol='predictedLabel', labels=stringIndexer.labels)
+
+# COMMAND ----------
+
+df2.select("ptProperty_doub", "ptProperty_ohe").show()
 
 
+# COMMAND ----------
+
+# encoder.transform(td).head().features
+# en = encoder.transform(td)#.head().features
+# en.printSchema()
+# en.select("ptProperty", "ptProperty_idx", "features").show(100)
+# en.agg({"ptProperty_idx": "max"}).collect()[0]
+
+encoder.setParams(outputCol="freqs").transform(td).head().freqs
+# en_0 = encoder.setParams(outputCol="freqs").transform(td)#.head().freqs
+# en_0.select("ptProperty", "ptProperty_idx", "freqs").show(100)
+# en_0.head().freqs
+
+params = {encoder.dropLast: False, encoder.outputCol: "test"}
+# encoder.transform(td, params).head().test
+en_1 = encoder.transform(td, params)#.head().test
+en_1.select("ptProperty", "ptProperty_idx", "test").show(100)
+
+onehotEncoderPath = temp_path + "/onehot-encoder"
+encoder.save(onehotEncoderPath)
+
+
+
+# COMMAND ----------
+
+loadedEncoder = OneHotEncoder.load(onehotEncoderPath)
+
+# loadedEncoder.getDropLast() == encoder.getDropLast()
+
+en_2 = loadedEncoder.transform(td, params)
+
+# COMMAND ----------
+
+en_2.select("ptProperty", "ptProperty_idx", "test").show(100)
+
+
+
+# model
+# modelPath = temp_path + "/ohe-model"
+# >>> model.save(modelPath)
+
+# COMMAND ----------
+
+# td.show(5)
+# td.select("ptProperty", "ptProperty_idx").show(100)
+td.printSchema()
+
+# COMMAND ----------
+
+def one_hot(series):
+    label_binarizer = pp.LabelBinarizer()
+    label_binarizer.fit(range(max(series)+1))
+    return label_binarizer.transform(series)
+
+# COMMAND ----------
+
+workClassIndexer = StringIndexer().setInputCol("workclass").setOutputCol("workclass_indexed")
+workClassOneHot =  OneHotEncoder().setInputCol("workclass_indexed").setOutputCol("workclass_onehot")
+salaryIndexer = StringIndexer().setInputCol("salary").setOutputCol("label")
+
+vectorAssembler = VectorAssembler().setInputCols(['workclass_onehot','age']).setOutputCol("features")
+# create pipeline
+pipeline = Pipeline().setStages([workClassIndexer,workClassOneHot, salaryIndexer,vectorAssembler])
+
+
+
+
+# COMMAND ----------
+
+df = sqlContext.createDataFrame([
+   (Vectors.dense([1, 2, 0]),),
+   (Vectors.dense([2, 0, 1]),),
+   (Vectors.dense([0, 1, 0]),)], ("features", ))
+
+# pca = PCA(k=2, inputCol="features", outputCol="pca")
+# model = pca.fit(df)
+# transformed = model.transform(df)
+
+# COMMAND ----------
+
+temp = portrait_df.rdd.map(lambda x:[float(y) for y in x['all_features']]).toDF(portrait_df.columns)
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
 
 
