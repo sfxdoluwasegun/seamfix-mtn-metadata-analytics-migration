fd01e2ac53053fac642d3ee15249041ef8cf2e0e
Fortune Okorji <fokorji@seamfix.com>
2019-09-09 18:18:08 +0100

    #DSC-389 Continuation on encoding categorical data to non-categorical data.

--- src/feature_scaling.py
+++ src/feature_scaling.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+import numpy
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.functions import *
 from pyspark.ml.feature import PCA, StringIndexer, VectorAssembler, OneHotEncoder, IndexToString
@@ -36,185 +37,93 @@ usecase_df = spark.read.json("s3n://seamfix-machine-learning/mtn_xmlmetadata/use
 # COMMAND ----------
 
 def get_df_name(df: DataFrame) -> str:
-    col_name =[x for x in globals() if globals()[x] is df][0]
-    return col_name
-
-def get_unique_items(df: DataFrame):
+    return [x for x in globals() if globals()[x] is df][0]
+  
+def get_df_unique_items(df: DataFrame):
   print("\n{} DataFrame \n".format(get_df_name(df)))
   for column in df.columns:
     print("{} - {}".format(column, df.toPandas()[column].unique()))
+    
+def get_column_unique_items(df: DataFrame, col: str) -> numpy.ndarray:
+  return df.toPandas()[col].unique()
+
+def get_column_idx(df: DataFrame, column_name: str) -> (str, DataFrame):
+  for (col, i) in zip(df.columns, range(len(df.columns))):
+    if col == column_name:
+      column_idx = "{}.columns[{}]".format(get_df_name(df), i)
+  return eval(column_idx), eval(column_idx.split(".")[0])
+
+def encoder(col_name: str, df: DataFrame) -> (DataFrame, DataFrame):
+  stringIndexer = StringIndexer(inputCol=col_name, outputCol="{}_{}".format(col_name, "doub"))
+  decimalModel = stringIndexer.fit(df)
+  str_double = decimalModel.transform(df)
+  str_double = str_double.drop(col_name)
+  encoder = OneHotEncoder(inputCol="{}_{}".format(col_name, "doub"), outputCol="{}_{}".format(col_name, "ohe"))
+  ohe = encoder.transform(str_double)
+  ohe = ohe.drop(col_name)
+  
+  onehotEncoderPath = temp_path + "/onehotEncoder"
+#   encoder.save(onehotEncoderPath)
+  encoder.write().overwrite().save(onehotEncoderPath)
+  
+  stringIndexerPath = temp_path + "/stringIndexer"
+#   stringIndexer.save(stringIndexerPath)
+  stringIndexer.write().overwrite().save(stringIndexerPath)
+  return str_double, ohe
+
 
 # COMMAND ----------
 
 df_list = [liveliness_df, portrait_df, fingerprints_df, usecase_df]
 for item in df_list:
   # Check unique_values.txt for detailed outline of unique values in each column
-  get_unique_items(item)
-
-# COMMAND ----------
-
-def encoder(col_name: str, df: DataFrame) -> DataFrame:
-  stringIndexer = StringIndexer(inputCol=col_name, outputCol="{}_{}".format(col_name, "doub"))
-  decimalModel = stringIndexer.fit(df)
-  str_double = decimalModel.transform(df)
-  encoder = OneHotEncoder(inputCol="{}_{}".format(col_name, "doub"), outputCol="{}_{}".format(col_name, "ohe"))
-  ohe = encoder.transform(str_double)
+  get_df_unique_items(item)
   
-  onehotEncoderPath = temp_path + "/onehot-encoder"
-#   encoder.save(onehotEncoderPath)
-  encoder.write().overwrite().save(onehotEncoderPath)
-  
-  stringIndexerPath = temp_path + "/ohe-model"
-#   stringIndexer.save(stringIndexerPath)
-  stringIndexer.write().overwrite().save(stringIndexerPath)
-  return str_double, ohe
-
-
 
 # COMMAND ----------
 
-def get_column_idx(df: DataFrame, column_name: str) -> str:
-  for (col, i) in zip(df.columns, range(len(df.columns))):
-    if col == column_name:
-      column_idx = "{}.columns[{}]".format(get_df_name(df), i)
-  return column_idx 
+get_column_unique_items(fingerprints_df, "fpDeviceType14")
 
 # COMMAND ----------
 
-get_column_idx(portrait_df, "location")
-
-
-# COMMAND ----------
-
-df1, df2 = encoder(portrait_df.columns[5], portrait_df)
+col_idx, dataframe = get_column_idx(fingerprints_df, "fpDeviceType0")
+df1, _ = encoder(col_idx, dataframe)
 
 # COMMAND ----------
 
-stringIndexer = StringIndexer(inputCol=portrait_df.columns[5], outputCol="{}_{}".format(portrait_df.columns[5], "doub"))
-decimalModel = stringIndexer.fit(fingerprints_df)
+stringIndexerPath = temp_path + "/stringIndexer"
+loadedIndexer = StringIndexer.load(stringIndexerPath)
+decimalModel = loadedIndexer.fit(fingerprints_df)
 str_double = decimalModel.transform(fingerprints_df)
 
 # COMMAND ----------
 
-type(stringIndexer) #pyspark.ml.feature.StringIndexer
-type(decimalModel) #pyspark.ml.feature.StringIndexerModel
-type(str_double) #pyspark.sql.dataframe.DataFrame
+str_double.printSchema()
 
 # COMMAND ----------
 
-# def reverse_encoder() 
-stringIndexerPath = temp_path + "/ohe-model"
-loadedIndexer = StringIndexer.load(stringIndexerPath)
-decimalModel = loadedIndexer.fit(usecase_df)
-str_double = decimalModel.transform(usecase_df)
-
-
-
-# convertor = IndexToString(inputCol='df.columns[-2]', outputCol='predictedLabel', labels=stringIndexer.labels)
-
-# COMMAND ----------
-
-liveliness_df.select(liveliness_df.columns[-1]).show()
-
-# COMMAND ----------
-
-# type(loadedIndexer) #pyspark.ml.feature.StringIndexer
+# type(stringIndexer) #pyspark.ml.feature.StringIndexer
 # type(decimalModel) #pyspark.ml.feature.StringIndexerModel
 # type(str_double) #pyspark.sql.dataframe.DataFrame
 
 # COMMAND ----------
 
-df2.select("ptProperty_doub", "ptProperty_ohe").show()
+# workClassIndexer = StringIndexer().setInputCol("workclass").setOutputCol("workclass_indexed")
+# workClassOneHot =  OneHotEncoder().setInputCol("workclass_indexed").setOutputCol("workclass_onehot")
+# salaryIndexer = StringIndexer().setInputCol("salary").setOutputCol("label")
+
+# vectorAssembler = VectorAssembler().setInputCols(['workclass_onehot','age']).setOutputCol("features")
+# # create pipeline
+# pipeline = Pipeline().setStages([workClassIndexer,workClassOneHot, salaryIndexer,vectorAssembler])
 
 
 # COMMAND ----------
 
-# encoder.transform(td).head().features
-# en = encoder.transform(td)#.head().features
-# en.printSchema()
-# en.select("ptProperty", "ptProperty_idx", "features").show(100)
-# en.agg({"ptProperty_idx": "max"}).collect()[0]
-
-encoder.setParams(outputCol="freqs").transform(td).head().freqs
-# en_0 = encoder.setParams(outputCol="freqs").transform(td)#.head().freqs
-# en_0.select("ptProperty", "ptProperty_idx", "freqs").show(100)
-# en_0.head().freqs
-
-params = {encoder.dropLast: False, encoder.outputCol: "test"}
-# encoder.transform(td, params).head().test
-en_1 = encoder.transform(td, params)#.head().test
-en_1.select("ptProperty", "ptProperty_idx", "test").show(100)
-
-onehotEncoderPath = temp_path + "/onehot-encoder"
-encoder.save(onehotEncoderPath)
-
-
-
-# COMMAND ----------
-
-loadedEncoder = OneHotEncoder.load(onehotEncoderPath)
-
-# loadedEncoder.getDropLast() == encoder.getDropLast()
-
-en_2 = loadedEncoder.transform(td, params)
-
-# COMMAND ----------
-
-en_2.select("ptProperty", "ptProperty_idx", "test").show(100)
-
-
-
-# model
-# modelPath = temp_path + "/ohe-model"
-# >>> model.save(modelPath)
-
-# COMMAND ----------
-
-# td.show(5)
-# td.select("ptProperty", "ptProperty_idx").show(100)
-td.printSchema()
-
-# COMMAND ----------
-
-def one_hot(series):
-    label_binarizer = pp.LabelBinarizer()
-    label_binarizer.fit(range(max(series)+1))
-    return label_binarizer.transform(series)
-
-# COMMAND ----------
-
-workClassIndexer = StringIndexer().setInputCol("workclass").setOutputCol("workclass_indexed")
-workClassOneHot =  OneHotEncoder().setInputCol("workclass_indexed").setOutputCol("workclass_onehot")
-salaryIndexer = StringIndexer().setInputCol("salary").setOutputCol("label")
-
-vectorAssembler = VectorAssembler().setInputCols(['workclass_onehot','age']).setOutputCol("features")
-# create pipeline
-pipeline = Pipeline().setStages([workClassIndexer,workClassOneHot, salaryIndexer,vectorAssembler])
-
-
-
-
-# COMMAND ----------
-
-df = sqlContext.createDataFrame([
-   (Vectors.dense([1, 2, 0]),),
-   (Vectors.dense([2, 0, 1]),),
-   (Vectors.dense([0, 1, 0]),)], ("features", ))
+# df = sqlContext.createDataFrame([
+#    (Vectors.dense([1, 2, 0]),),
+#    (Vectors.dense([2, 0, 1]),),
+#    (Vectors.dense([0, 1, 0]),)], ("features", ))
 
 # pca = PCA(k=2, inputCol="features", outputCol="pca")
 # model = pca.fit(df)
 # transformed = model.transform(df)
-
-# COMMAND ----------
-
-temp = portrait_df.rdd.map(lambda x:[float(y) for y in x['all_features']]).toDF(portrait_df.columns)
-
-
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-
