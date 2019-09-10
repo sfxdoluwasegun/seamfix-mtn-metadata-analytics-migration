7090d92ae65a9168f427452769ddda2db79ce2f7
Babatunde Adekunle <badekunle@seamfix.com>
2019-09-10 18:01:05 +0100

    Some commit for Jacintha

--- Load_data.py
+++ Load_data.py
@@ -1,10 +1,9 @@
-# Necessary libraries to be installed are: dask, flatten_json, json(comes pre-installed)
+import json
+import pandas as pd
+import flatten_json
+from flatten_json import flatten
 
 if __name__ == '__main__':
-    import json
-    import pandas as pd
-    import flatten_json
-    from flatten_json import flatten
     print('All libraries in')
     data = []
     with open('METRIC_DATA_REPORT_NEW_view.json') as f:
@@ -17,8 +16,12 @@ if __name__ == '__main__':
     df = pd.DataFrame(first_set)
     df.to_csv('part1.csv', index=False)
     print('... first batch of meta data records have been saved to the local drive')
-    second_set = flattened_list[20000:]
+    second_set = flattened_list[20000: 34000]
     df = pd.DataFrame(second_set)
-    df.to_csv('part2.csv', index = False)
+    df.to_csv('part2a.csv', index = False)
     print('... second batch of meta data records have been saved to the local drive')
+    third_set = flattened_list[34000 : ]
+    df = pd.DataFrame(third_set)
+    df.to_csv('part2b.csv', index=False)
+    print('... third batch of meta data records have been saved to the local drive')
     print('--------- End of code ---------')
